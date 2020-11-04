065ffed1911e5be02c8e6f569b130d3c84f3d51c
Micael <micael.dossantos@edu.itescia.fr>
2020-11-04 21:48:33 +0100

    createUser / emailVerification / loginUser done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'Home.dart';
 
-final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-final TextEditingController _emailController = TextEditingController();
-final TextEditingController _passwordController = TextEditingController();
-bool _success;
-String _userEmail;
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
 void main() async {
@@ -38,56 +34,33 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Pet\'s Meet'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: Builder(builder: (BuildContext context) {
+        return ListView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.all(16),
           children: <Widget>[
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              alignment: Alignment.center,
-              child: RaisedButton(
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    _register();
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Text(_success == null
-                  ? ''
-                  : (_success
-                      ? 'Successfully registered ' + _userEmail
-                      : 'Registration failed')),
-            )
+            _RegisterEmailSection(),
+            _EmailPasswordForm(),
           ],
-        ),
-      ),
+        );
+      }),
     );
   }
+}
+
+class _RegisterEmailSection extends StatefulWidget {
+  @override
+  _RegisterEmailSectionState createState() => _RegisterEmailSectionState();
+}
+
+class _RegisterEmailSectionState extends State<_RegisterEmailSection> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool hasClick;
+  String createAccountMessage;
+  bool _success;
+  String _userEmail;
 
   void dispose() {
     _emailController.dispose();
@@ -95,21 +68,222 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Veuillez entrer votre email.';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Veuillez entrer votre mot de passe.';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  hasClick = true;
+                  _register();
+                }
+              },
+              child: const Text('Créer un compte'),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: hasClick == true
+                ? Text(
+                    createAccountMessage,
+                    style: TextStyle(color: Colors.red),
+                  )
+                : Text(''),
+          )
+        ],
+      ),
+    );
+  }
+
   void _register() async {
+    if (_passwordController.text.length < 6) {
+      setState(() {
+        createAccountMessage =
+            'Entrez un mot de passe de plus de 6 caractères.';
+      });
+    }
     final User user = (await _auth.createUserWithEmailAndPassword(
       email: _emailController.text,
       password: _passwordController.text,
     ))
         .user;
-    print(user);
+
+    try {
+      await user.sendEmailVerification();
+    } catch (e) {
+      print("An error occured while trying to send email verification");
+      print(e.message);
+    }
     if (user != null) {
       setState(() {
         _success = true;
-        _userEmail = user.email;
+        _userEmail = _emailController.text;
+        _emailController.text = '';
+        _passwordController.text = '';
       });
     } else {
       setState(() {
-        _success = true;
+        _success = false;
+      });
+    }
+
+    if (_success) {
+      setState(() {
+        createAccountMessage =
+            'Votre compte à bien été créé, veuillez consulter votre boite mail !';
+      });
+    } else {
+      setState(() {
+        createAccountMessage = 'Une erreur est survenue, veuillez réessayer.';
+      });
+    }
+  }
+}
+
+class _EmailPasswordForm extends StatefulWidget {
+  @override
+  _EmailPasswordFormState createState() => _EmailPasswordFormState();
+}
+
+class _EmailPasswordFormState extends State<_EmailPasswordForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _success = false;
+  String _userEmail;
+  bool hasClick = false;
+
+  String createAccountMessage;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: const Text('Test sign in with email and password'),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Veuillez entrer votre email.';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Veuillez entrer votre mot de passe.';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              child: const Text('Se connecter'),
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  hasClick = true;
+                  _signInWithEmailAndPassword();
+                }
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: hasClick == true
+                ? Text(
+                    createAccountMessage,
+                    style: TextStyle(color: Colors.red),
+                  )
+                : Text(''),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _signInWithEmailAndPassword() async {
+    if (_passwordController.text.length < 6) {
+      setState(() {
+        createAccountMessage =
+            'Entrez un mot de passe de plus de 6 caractères.';
+      });
+    }
+    final User user = (await _auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+
+    if (user != null) {
+      if (user.emailVerified) {
+        print(user.emailVerified);
+        setState(() {
+          _success = true;
+          _userEmail = user.email;
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Home()));
+        });
+      } else {
+        _emailController.text = '';
+        _passwordController.text = '';
+        setState(() {
+          _success = false;
+        });
+      }
+    } else {
+      setState(() {
+        _success = false;
+      });
+    }
+    if (_success) {
+      setState(() {
+        createAccountMessage = 'Authentification réussi !';
+      });
+    } else {
+      setState(() {
+        createAccountMessage =
+            'Authentification échoué. Créer un compte ou consulter votre boite mail afin de valider votre compte';
       });
     }
   }
