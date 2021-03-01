02d8d16c034ec4d6cae69e1b2edb4abcc52424e7
Riyad <riyad.achour@edu.itesica.fr>
2021-03-01 12:32:39 +0100

    News page create

---
+++ lib/widgets/newsFeeds.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('News Page'),
+    ));
+  }
+}
