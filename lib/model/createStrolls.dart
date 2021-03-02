fc5ee823d23cab724eeebf39fa2319cfc6470841
Riyad <riyad.achour@edu.itesica.fr>
2021-03-02 18:13:57 +0100

    create strolls

---
+++ lib/model/createStrolls.dart
@@ -0,0 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class CreateStrolls extends StatefulWidget {
+  CreateStrolls({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _CreateStrolls();
+  }
+}
+
+class _CreateStrolls extends State<CreateStrolls> {
+  String currentUser;
+  final TextEditingController _creatorController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool hasClick = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: Column(children: <Widget>[
+        TextFormField(
+          controller: _creatorController,
+          decoration: const InputDecoration(labelText: 'Nom'),
+          validator: (String value) {
+            if (value.isEmpty) {
+              return 'Veuillez entrer votre nom.';
+            }
+            return null;
+          },
+        ),
+        TextFormField(
+          controller: _descriptionController,
+          decoration: const InputDecoration(labelText: 'Description'),
+          validator: (String value) {
+            return null;
+          },
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            child: const Text('Créer balade'),
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                hasClick = true;
+                createScroll(
+                    _creatorController.text, _descriptionController.text);
+              }
+            },
+          ),
+        )
+      ]),
+    ));
+  }
+
+  Future<void> createScroll(String creator, String description) async {
+    CollectionReference users =
+        FirebaseFirestore.instance.collection('strolls');
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser.uid.toString();
+    users.add({'creator': creator, 'description': description, 'uid': uid});
+    return;
+  }
+}
--- lib/widgets/newsFeeds.dart
+++ lib/widgets/newsFeeds.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class News extends StatelessWidget {
@@ -18,10 +19,40 @@ class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("News Page"),
-      ),
-      body: Text("Hello World"),
+        appBar: AppBar(
+          title: Text("News Page"),
+        ),
+        body: GetNews('3ZMmsftcrL9DIdGUB1Hj'));
+  }
+
+  void getUser() {}
+}
+
+class GetNews extends StatelessWidget {
+  final String documentId;
+
+  GetNews(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference strolls =
+        FirebaseFirestore.instance.collection('strolls');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: strolls.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Balade proposé par ${data['creator']} ");
+        }
+
+        return Text("loading");
+      },
     );
   }
 }
