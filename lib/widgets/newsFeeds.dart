5ebc42480eec8e0fe1e8e4dbe75c5eaa23e88b5b
Riyad <riyad.achour@edu.itesica.fr>
2021-03-02 15:46:47 +0100

    newsPage create

--- lib/widgets/newsFeeds.dart
+++ lib/widgets/newsFeeds.dart
@@ -3,9 +3,25 @@ import 'package:flutter/material.dart';
 class News extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(title: "news", home: NewsPage());
+  }
+}
+
+class NewsPage extends StatefulWidget {
+  NewsPage({Key key}) : super(key: key);
+
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('News Page'),
-    ));
+      appBar: AppBar(
+        title: Text("News Page"),
+      ),
+      body: Text("Hello World"),
+    );
   }
 }
