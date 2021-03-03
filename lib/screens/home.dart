286f6815da529c0a3414633ac9563346baba7081
Riyad <riyad.achour@edu.itesica.fr>
2021-03-03 19:23:46 +0100

    modal create

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -15,14 +16,149 @@ class _Home extends State<Home> {
   String currentUser;
   final TextEditingController _creatorController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _placeController = TextEditingController();
+  final TextEditingController _participantsController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   bool hasClick = false;
+  int stroll_id = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return Container(
+      child: FlatButton(
+          child: Text("Créer Balade"),
+          onPressed: () {
+            return showGeneralDialog(
+              barrierLabel: "Barrier",
+              barrierDismissible: true,
+              barrierColor: Colors.black.withOpacity(0.5),
+              transitionDuration: Duration(milliseconds: 700),
+              context: context,
+              pageBuilder: (_, __, ___) {
+                return Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    height: 600,
+                    child: SizedBox.expand(
+                        child: Form(
+                      key: _formKey,
+                      child: Scaffold(
+                          appBar: AppBar(
+                            title: Text('Créer une balade'),
+                          ),
+                          body: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            child: Form(
+                              child: Column(children: <Widget>[
+                                TextFormField(
+                                  controller: _creatorController,
+                                  decoration:
+                                      const InputDecoration(labelText: 'Nom'),
+                                  validator: (String value) {
+                                    if (value.isEmpty) {
+                                      return 'Veuillez entrer votre nom.';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                TextFormField(
+                                  controller: _descriptionController,
+                                  decoration: const InputDecoration(
+                                      labelText: 'Description'),
+                                  validator: (String value) {
+                                    return null;
+                                  },
+                                ),
+                                TextFormField(
+                                  controller: _participantsController,
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                      labelText: 'Nombre de participants'),
+                                  validator: (String value) {
+                                    if (value.isEmpty) {
+                                      return 'Veuillez entrer un nombre de participants';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                TextFormField(
+                                  controller: _placeController,
+                                  decoration:
+                                      const InputDecoration(labelText: 'Place'),
+                                  validator: (String value) {
+                                    if (value.isEmpty) {
+                                      return 'Veuillez entrer une ville';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                /*  TextFormField(
+                                  controller: _dateController,
+                                  decoration:
+                                      const InputDecoration(labelText: 'Date'),
+                                  validator: (String value) {
+                                    return null;
+                                  },
+                                ),*/
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 16.0),
+                                  alignment: Alignment.center,
+                                  child: RaisedButton(
+                                    child: const Text('Créer balade'),
+                                    onPressed: () async {
+                                      if (_formKey.currentState.validate()) {
+                                        // ignore: unnecessary_statements
+                                        stroll_id++;
+                                        hasClick = true;
+                                        if (_creatorController
+                                                .text.isNotEmpty ||
+                                            _descriptionController
+                                                .text.isNotEmpty ||
+                                            _participantsController
+                                                .text.isNotEmpty ||
+                                            _placeController.text.isNotEmpty) {
+                                          createScroll(
+                                              _auth.currentUser.uid,
+                                              _creatorController.text,
+                                              _descriptionController.text,
+                                              _participantsController.text,
+                                              _placeController.text,
+                                              stroll_id.toString());
+                                        }
+                                      }
+                                    },
+                                  ),
+                                )
+                              ]),
+                            ),
+                          )),
+                    )),
+                    margin: EdgeInsets.only(bottom: 50, left: 12, right: 12),
+                  ),
+                );
+              },
+              transitionBuilder: (_, anim, __, child) {
+                return SlideTransition(
+                  position: Tween(begin: Offset(0, 1), end: Offset(0, 0))
+                      .animate(anim),
+                  child: child,
+                );
+              },
+            );
+            ;
+          }),
+    );
+    /*return Form(
       key: _formKey,
       child: Scaffold(
           appBar: AppBar(
@@ -65,16 +201,22 @@ class _Home extends State<Home> {
               ]),
             ),
           )),
-    );
+    ); */
   }
 
-  Future<void> createScroll(
-      String creator, String description, String uid) async {
+  Future<void> createScroll(String uid, String creator, String description,
+      String participants, String place, String strollid) async {
     CollectionReference users =
         FirebaseFirestore.instance.collection('strolls');
-    //FirebaseAuth auth = FirebaseAuth.instance;
-    //String uid = auth.currentUser.uid.toString();
-    users.add({'creator': creator, 'description': description, 'uid': uid});
+    users.add({
+      'creator_uid': uid,
+      'creator': creator,
+      'description': description,
+      'place': place,
+      'participant': participants,
+      // 'date': date,
+      'stroll_id': strollid
+    });
     return;
   }
 }
