f797d3fb66b331fedb8a65531cc4c7949f531b72
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2021-03-05 10:33:21 +0100

    add map && settings

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -1,10 +1,84 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Maps extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map/map.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:map/map.dart';
+
+class Maps extends StatefulWidget {
+  @override
+  _MapsState createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+  static final CameraPosition _initialPoition = CameraPosition(
+      target: LatLng(48.82930066474196, 2.2875759067459267), zoom: 15);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("maps"),
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPoition,
+      ),
     );
   }
 }
+// class Maps extends StatefulWidget {
+//   @override
+//   _MapsState createState() => _MapsState();
+// }
+
+// class _MapsState extends State<Maps> {
+//   GoogleMapController _controller;
+
+//   final CameraPosition _initialPosition =
+//       CameraPosition(target: LatLng(24.903623, 67.198367));
+
+//   final List<Marker> markers = [];
+
+//   addMarker(cordinate) {
+//     int id = Random().nextInt(100);
+
+//     setState(() {
+//       markers
+//           .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GoogleMap(
+//         initialCameraPosition: _initialPosition,
+//         mapType: MapType.normal,
+//         onMapCreated: (controller) {
+//           setState(() {
+//             _controller = controller;
+//           });
+//         },
+//         markers: markers.toSet(),
+//         onTap: (cordinate) {
+//           _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+//           addMarker(cordinate);
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           _controller.animateCamera(CameraUpdate.zoomOut());
+//         },
+//         child: Icon(Icons.zoom_out),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+
+// class Maps extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Text("maps"),
+//     );
+//   }
+// }
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,10 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pets_meet/routes.dart';
+import 'package:pets_meet/routing.dart';
+import 'package:pets_meet/screens/connection.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Setting'),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: ListView(
+          children: [
+            Text(
+              "Réglages",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Account",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            buildAccountOptionRow(context, "Changer votre mot de passe"),
+            buildAccountOptionRow(context, "Langue"),
+            buildAccountOptionRow(context, "Confidentialité et sécurité"),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.volume_up_outlined,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Notifications",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            buildNotificationOptionRow("Notification", true),
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              // ignore: deprecated_member_use
+              child: OutlineButton(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {
+                  Routing.navigateToScreen(context, Routes.Connection);
+                },
+                child: Text("DECONNEXION",
+                    style: TextStyle(
+                        fontSize: 16, letterSpacing: 2.2, color: Colors.black)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildNotificationOptionRow(String title, bool isActive) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600]),
+        ),
+        Transform.scale(
+            scale: 0.7,
+            child: CupertinoSwitch(
+              value: isActive,
+              onChanged: (bool val) {},
+            ))
+      ],
+    );
+  }
+
+  GestureDetector buildAccountOptionRow(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Option 1"),
+                    Text("Option 2"),
+                    Text("Option 3"),
+                  ],
+                ),
+                actions: [
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Close")),
+                ],
+              );
+            });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
