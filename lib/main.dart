f1e4b1124d2c0832bf6b2835c956af048bfedf0e
Micael <micael.dossantos@edu.itescia.fr>
2021-03-05 10:27:06 +0100

    fix stroll details

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase Auth Demo',
-      home: Register(),
+      home: Navigation(),
     );
   }
 }
