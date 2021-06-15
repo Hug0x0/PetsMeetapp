06b3a119d1cdba6732265bfbd735984d13a3eb5a
Achour Riyad <achourriyad13@gmail.com>
2021-06-15 10:06:46 +0200

    Firebase performance

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,19 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCXRgdm5ZUb8ZQW463Bys4ROWBUr2lmpAU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    FirebaseApp.configure()
+
   }
 }
